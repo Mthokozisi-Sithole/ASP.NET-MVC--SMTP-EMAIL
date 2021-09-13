ad084a1a729aa4a79aa4d20449edfd7cf8aa81b3
mthokozisi sithole <mthokozisiephraim@gmail.com>
2021-09-13 09:49:17 +0200

    Just Pushed my Base Code.

---
+++ SendingEmail/Models/EmailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendingEmail.Models
+{
+    public class EmailViewModel
+    {
+        [Required]
+        public string EmailSubject { get; set; }
+
+        public string EmailBody { get; set; }
+
+        [Required]
+        public string ReceiverEmailAddress { get; set; }
+        public string SenderEmailAddress { get; set; }
+        public string SenderPassword { get; set; }
+        public string SmtpHostServer { get; set; }
+        public int SmtpPort { get; set; }
+    }
+}
